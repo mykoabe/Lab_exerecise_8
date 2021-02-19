133e0dc2abf4c958da2ed94047bb8683aeac3a94
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-02-19 22:55:17 +0300

    I have done the export and import

--- asset/js/07(Modules_Export).js
+++ asset/js/07(Modules_Export).js
@@ -1,7 +1,6 @@
-//1. Add export key work before const  
+//1. Add export key word before const  
 const myVar = "Export Var By Adding Export Keyword In the Front!!";
-
-
+export{myVar}
 
 //export this function at last line
 function exp1() {
@@ -16,9 +15,11 @@ function exp3() {
     return "Import as an Alias!!!";
 }
 //2. List export(comma separated list )
-
+export {exp1, exp2, exp3}
 
 //3. Export exp2 with AliasName ==> exp2_Alias
+export{exp2 as exp2_Alias};
 
+// //4. Exporting exp3
+// export{exp3};  //it says exporting multiple time as it is exported at the upper stage
 
-//4. Export exp3
