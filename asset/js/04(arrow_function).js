ca644019473758cb78b3792d61bb8084dbff3fe7
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-02-19 15:49:56 +0300

    arrow function finished

--- asset/js/04(arrow_function).js
+++ asset/js/04(arrow_function).js
@@ -13,11 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
 //1. Replace the empty string with arrow function 
 
 //2. An arrow function with full syntax
-const arrowFun1 = '';
+const arrowFun1 = (x,y) =>{
+    return x+y
+}
 
 //3. A minimized arrow function [No return statement , no curly brace]
-const arrowFun2 = '';
+const arrowFun2 = (x,y)=> x + y;
 
 //4. A minimized arrow function [No return statement , no curly brace, no square bracket]
 //5. The function will return the sqrt , use Math.sqrt(var);
-const arrowFun3 = '';
+const arrowFun3 = x => Math.sqrt(x);
